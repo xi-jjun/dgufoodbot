fdc0e9b4ee8d821cc9a7b1630b4aa7b1ede316db
xi_jjun <rlawowns97@naver.com>
2022-10-18 22:20:29 +0900

    project init

---
+++ src/main/java/jjun/toyproject/dgufoodbot/DgufoodbotApplication.java
@@ -0,0 +1,13 @@
+package jjun.toyproject.dgufoodbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgufoodbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgufoodbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jjun/toyproject/dgufoodbot/DgufoodbotApplicationTests.java
@@ -0,0 +1,13 @@
+package jjun.toyproject.dgufoodbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgufoodbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
